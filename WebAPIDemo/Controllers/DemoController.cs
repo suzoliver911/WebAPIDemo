fa7b05b107cb55c9e217c379ddd070865e857fe3
Oliver <Oliver.Suzanne2@mayo.edu>
2018-10-24 13:12:26 -0500

    added Demo controller

---
+++ WebAPIDemo/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+
+        public List<string> Get(int Id)
+        {
+            return new List<string>
+            {
+                "Data1",
+                "Data2"
+            };
+        }
+    }
+}
