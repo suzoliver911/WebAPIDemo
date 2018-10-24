898faed5b0e860235ec7b841746f24c94e295d93
Oliver <Oliver.Suzanne2@mayo.edu>
2018-10-24 13:30:45 -0500

    added swagger api documentation

--- WebAPIDemo/Controllers/DemoController.cs
+++ WebAPIDemo/Controllers/DemoController.cs
@@ -9,11 +9,28 @@ namespace WebAPIDemo.Controllers
 {
     public class DemoController : ApiController
     {
-        public string Get()
+        /// <summary>
+        /// My super fun demo get the whole world
+        /// </summary>
+        /// <remarks>
+        /// Get a super fun message
+        /// </remarks>
+        /// <returns>
+        /// Returns a super fun welcome message
+        /// </returns>
+        /// <returns></returns>
+       public string Get()
         {
             return "Welcome To Web API";
         }
 
+        /// <summary>
+        /// Another super fun demo get by id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns>
+        /// Returns a random list of fun
+        /// </returns>
         public List<string> Get(int Id)
         {
             return new List<string>
